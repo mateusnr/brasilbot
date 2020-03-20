4a7f723b390d8b7662094ae3697e0131cb20b3db
Gabriel Chiconi <gabriel.lins@bettatecnologia.com.br>
2020-03-20 18:05:31 -0300

    fix double try clause

--- src/workers/corona.ts
+++ src/workers/corona.ts
@@ -8,23 +8,19 @@ const CORONA_CHANNEL = 'covid-19'
 
 export const monitorCorona = async (client: Discord.Client, logger: Winston.Logger) => {
   try {
-    try {
-      const brData = await getCorona('brazil')
-      const guild = client.guilds.cache.get(config.discord.guild_id)
-      if (!guild) {
-        return logger.warn(`Could not find guild with id ${config.discord.guild_id}`);
-      }
-  
-      const channel = guild.channels.cache.find(channel => channel.name === CORONA_CHANNEL) as Discord.TextChannel;
-      if (!channel) {
-        return logger.warn(`Could not find channel with name ${config.discord.channel}`);
-      }
-      await channel.send(brData);
-    } catch (err) {
-      logger.warn(err)
+    const brData = await getCorona('brazil')
+    const guild = client.guilds.cache.get(config.discord.guild_id)
+    if (!guild) {
+      return logger.warn(`Could not find guild with id ${config.discord.guild_id}`);
     }
+
+    const channel = guild.channels.cache.find(channel => channel.name === CORONA_CHANNEL) as Discord.TextChannel;
+    if (!channel) {
+      return logger.warn(`Could not find channel with name ${config.discord.channel}`);
+    }
+    await channel.send(brData);
   } catch (err) {
-    logger.warn(err);
+    logger.warn(err)
   }
   return;
 }

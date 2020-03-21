adc342b427aa99e341b1eeef580718359ecc5fe9
Mateus Nóbrega <mateusn@outlook.com>
2020-03-21 00:50:13 -0300

    Fixing indentation and filtering diacritics from input

--- src/functions/get-corona.ts
+++ src/functions/get-corona.ts
@@ -5,39 +5,44 @@ const CORONA_MONITOR = 'http://bing.com/covid/data/?setlang=pt-br';
 const VIRUS_ICON = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.ftkmj8qzG4U3MUQnptxqoAHaHa%26pid%3DApi&f=1';
 
 interface BingResponse {
-  data: {
-    areas: {
-      id: string
-      displayName: string
-      totalConfirmed: number
-      totalDeaths: number
-      totalRecovered: number
-      lastUpdated: string
-    }[]
-  }
+    data: {
+        areas: {
+            id: string
+            displayName: string
+            totalConfirmed: number
+            totalDeaths: number
+            totalRecovered: number
+            lastUpdated: string
+        }[]
+    }
 };
 
-export async function getCorona (countryName: string) {
-  const { data: { areas } }: BingResponse = await Axios.get(CORONA_MONITOR)
-  const areaData = areas.find(a => a.displayName.toLowerCase() === countryName.toLowerCase());
+function removeDiacritics(str: String) {
+    return str.toLowerCase().normalize("NFKD").replace(/[^\w\s]/g, '');
+}
 
-  if (!areaData) {
+export async function getCorona(countryName: string) {
+    const { data: { areas } }: BingResponse = await Axios.get(CORONA_MONITOR)
+    const filteredCountryName = removeDiacritics(countryName)
+    const areaData = areas.find(a => removeDiacritics(a.displayName) === filteredCountryName);
+
+    if (!areaData) {
+        const embed = new Discord.MessageEmbed()
+            .setTitle("Não encontrei dados para `" + countryName + "`!")
+            .setFooter(`Para ver esta mensagem a qualquer momento, digite \`!covid countryname\`.`);
+
+        return embed
+    }
+
+    const readableLastUpdated = new Date(areaData.lastUpdated).toLocaleString('pt-BR');
+
     const embed = new Discord.MessageEmbed()
-      .setTitle("Não encontrei dados para `" + countryName + "`!")
-      .setFooter(`Para ver esta mensagem a qualquer momento, digite \`!covid countryname\`.`);
+        .setAuthor("Últimas atualizações sobre o Coronavírus", VIRUS_ICON)
+        .setTitle(`Casos de Coronavírus: ${areaData.displayName}`)
+        .addField("Confirmados", areaData.totalConfirmed)
+        .addField("Recuperados", areaData.totalRecovered)
+        .addField("Mortes", areaData.totalDeaths)
+        .setFooter(`Última atualização em ${readableLastUpdated}.\nPara ver esta mensagem a qualquer momento, digite \`!covid countryname\`.`);
 
     return embed
-  }
-
-  const readableLastUpdated = new Date(areaData.lastUpdated).toLocaleString('pt-BR');
-
-  const embed = new Discord.MessageEmbed()
-    .setAuthor("Últimas atualizações sobre o Coronavírus", VIRUS_ICON)
-    .setTitle(`Casos de Coronavírus: ${areaData.displayName}`)
-    .addField("Confirmados", areaData.totalConfirmed)
-    .addField("Recuperados", areaData.totalRecovered)
-    .addField("Mortes", areaData.totalDeaths)
-    .setFooter(`Última atualização em ${readableLastUpdated}.\nPara ver esta mensagem a qualquer momento, digite \`!covid countryname\`.`);
-
-  return embed
 }
--- src/workers/corona.ts
+++ src/workers/corona.ts
@@ -7,20 +7,20 @@ import { getCorona } from '../functions/get-corona'
 const CORONA_CHANNEL = 'covid-19'
 
 export const monitorCorona = async (client: Discord.Client, logger: Winston.Logger) => {
-  try {
-    const brData = await getCorona('Brasil')
-    const guild = client.guilds.cache.get(config.discord.guild_id)
-    if (!guild) {
-      return logger.warn(`Could not find guild with id ${config.discord.guild_id}`);
-    }
+	try {
+		const brData = await getCorona('Brasil')
+		const guild = client.guilds.cache.get(config.discord.guild_id)
+		if (!guild) {
+			return logger.warn(`Could not find guild with id ${config.discord.guild_id}`);
+		}
 
-    const channel = guild.channels.cache.find(channel => channel.name === CORONA_CHANNEL) as Discord.TextChannel;
-    if (!channel) {
-      return logger.warn(`Could not find channel with name ${config.discord.channel}`);
-    }
-    await channel.send(brData);
-  } catch (err) {
-    logger.warn(err)
-  }
-  return;
+		const channel = guild.channels.cache.find(channel => channel.name === CORONA_CHANNEL) as Discord.TextChannel;
+		if (!channel) {
+			return logger.warn(`Could not find channel with name ${config.discord.channel}`);
+		}
+		await channel.send(brData);
+	} catch (err) {
+		logger.warn(err)
+	}
+	return;
 }

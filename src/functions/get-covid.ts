da2b88962aef0f02bac589a8865ec20dd3ef59a8
Mateus Nóbrega <mateusn@outlook.com>
2020-03-21 20:36:56 -0300

    Adding worldwide info, code refactoring

--- src/functions/get-covid.ts
+++ src/functions/get-covid.ts
@@ -4,48 +4,68 @@ import * as Discord from 'discord.js';
 const COVID_MONITOR_URL = 'http://bing.com/covid/data/?setlang=pt-br';
 const VIRUS_ICON = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.ftkmj8qzG4U3MUQnptxqoAHaHa%26pid%3DApi&f=1';
 
+interface AreaInfo {
+    id:				string;
+    displayName:	string;
+    totalConfirmed: number;
+    totalDeaths:	number;
+    totalRecovered: number;
+    lastUpdated:	string;
+}
+
 interface BingResponse {
     data: {
-        areas: {
-            id:				string
-            displayName:	string
-            totalConfirmed: number
-            totalDeaths:	number
-            totalRecovered: number
-            lastUpdated:	string
-		}[];
-		totalConfirmed:  number;
-		totalDeaths:     number;
-		totalRecovered:  number;
-    }
+        areas: AreaInfo[];
+        totalConfirmed:  number;
+        totalDeaths:     number;
+        totalRecovered:  number;
+        lastUpdated:	string;
+    };
 };
 
 function removeDiacritics(str: String) {
     return str.toLowerCase().normalize("NFKD").replace(/[^\w\s]/g, '');
 }
 
+function createCovidEmbed(bingData: BingResponse, areaData?: AreaInfo){
+    const totalConfirmed = areaData?.totalConfirmed || bingData.data.totalConfirmed;
+    const totalRecovered = areaData?.totalRecovered || bingData.data.totalRecovered;
+    const totalDeaths = areaData?.totalDeaths || bingData.data.totalDeaths;
+
+    const lastUpdated = new Date (areaData?.lastUpdated || bingData.data.lastUpdated).toLocaleString('pt-BR', { timeZone: 'America/Sao_Paulo'});
+
+    const embed = new Discord.MessageEmbed()
+        .setColor('RED')
+        .setAuthor("Últimas atualizações sobre o Coronavírus", VIRUS_ICON)
+        .setTitle(`Casos de Coronavírus: ${areaData?.displayName || "Global"}`)
+        .addField("Confirmados", totalConfirmed)
+        .addField("Recuperados", totalRecovered)
+        .addField("Mortes", totalDeaths)
+        .setFooter(`Última atualização em ${lastUpdated} BRT.\nPara ver esta mensagem a qualquer momento, digite \`!covid countryname\`.`);
+
+    return embed;
+
+}
+
 export async function getCovidData(countryName: string) {
-    const { data: { areas, totalConfirmed, totalDeaths, totalRecovered } }: BingResponse = await Axios.get(COVID_MONITOR_URL)
-    const filteredCountryName = removeDiacritics(countryName)
+    const bingData: BingResponse = await Axios.get(COVID_MONITOR_URL)
+    const { data: { areas } }: BingResponse = bingData;
+
+    const filteredCountryName = removeDiacritics(countryName);
     const areaData = areas.find(a => removeDiacritics(a.displayName) === filteredCountryName);
 
     if (!areaData) {
+        // user asked for worlwide info
+        if (filteredCountryName === "global")
+            return createCovidEmbed(bingData, areaData);
+
         const embed = new Discord.MessageEmbed()
             .setTitle("Não encontrei dados para `" + countryName + "`!")
             .setFooter(`Para ver esta mensagem a qualquer momento, digite \`!covid countryname\`.`);
 
-        return embed
+        return embed;
     }
 
-    const readableLastUpdated = new Date(areaData.lastUpdated).toLocaleString('pt-BR');
 
-    const embed = new Discord.MessageEmbed()
-        .setAuthor("Últimas atualizações sobre o Coronavírus", VIRUS_ICON)
-        .setTitle(`Casos de Coronavírus: ${areaData.displayName}`)
-        .addField("Confirmados", areaData.totalConfirmed)
-        .addField("Recuperados", areaData.totalRecovered)
-        .addField("Mortes", areaData.totalDeaths)
-        .setFooter(`Última atualização em ${readableLastUpdated}.\nPara ver esta mensagem a qualquer momento, digite \`!covid countryname\`.`);
-
-    return embed
+    return createCovidEmbed(bingData, areaData);
 }

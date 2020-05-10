da9361b03e8523046d027e99b9c148fb1c023afe
Mateus Nóbrega <mateusn@outlook.com>
2020-05-10 00:07:53 -0300

    Adding user mention support to the remove and add commands

--- src/commands/roles.ts
+++ src/commands/roles.ts
@@ -3,21 +3,37 @@ import config from '../config'
 import { CommandHandler } from '../command-handler'
 import { fail, removeDiacritics } from '../helpers/utils'
 
-const findRole = (message: Discord.Message, args: string[]): { role?: Discord.Role; roleName: string } => {
-    const roleName = args.join(' ')
+interface RoleCommandData {
+    role?: Discord.Role
+    roleName: string
+    user: Discord.GuildMember
+}
 
+const findRole = (message: Discord.Message, args: string[]): RoleCommandData => {
+    const userIdStr = args[0]
+    const mentionOrUserIDPattern = /<?@?!?(\d{17,19})>?/ // only one user can be mentioend, for now
+    const isAnyUserMentioned = mentionOrUserIDPattern.test(userIdStr)
+
+    // Checks if a user was mentioned to retrieve the role name
+    const roleName = (!isAnyUserMentioned ? args.join(' ') : args.slice(1).join(' '))
     const role = message.guild!.roles.cache.find(i => removeDiacritics(i.name) === removeDiacritics(roleName))
+    let user: Discord.GuildMember = message.member!
 
-    return { role, roleName }
+    if (isAnyUserMentioned) {
+        const userId: string = mentionOrUserIDPattern.exec(userIdStr)![1]
+        user = message.guild!.members.cache.get(userId)!
+    }
+
+    return { role, roleName, user }
 }
 
 type RoleCommandHandler =
-    (message: Discord.Message, role: Discord.Role) => Promise<void>
+    (message: Discord.Message, role: Discord.Role, user: Discord.GuildMember) => Promise<void>
 
 function checkRole (fn: RoleCommandHandler): CommandHandler {
     return async (message, args): Promise<void> => {
         try {
-            const { role, roleName } = findRole(message, args)
+            const { role, roleName, user } = findRole(message, args)
 
             if (!role) {
                 if (roleName === '') {
@@ -29,17 +45,17 @@ function checkRole (fn: RoleCommandHandler): CommandHandler {
                 return
             }
 
-            fn(message, role)
+            fn(message, role, user)
         } catch (err) {
             await message.channel.send(err.code)
             if (err.code === 50013) { // Missing permissions
-                message.reply('Não tenho permissões pra realizar essa ação')
+                fail(message, 'Não tenho permissões pra realizar essa ação')
             }
         }
     }
 }
 
-export const addRoleHandler: CommandHandler = checkRole(async (message, role) => {
+export const addRoleHandler: CommandHandler = checkRole(async (message, role, user) => {
     if (!config.roles.includes(role.name)) {
         return fail(message, `Você não pode adicionar a role **${role.name}**.`)
     }
@@ -50,18 +66,18 @@ export const addRoleHandler: CommandHandler = checkRole(async (message, role) =>
 
     await message.delete()
 
-    await message.member!.roles.add(role)
+    await user.roles.add(role)
 
     const msg = await message.channel.send(`A role **${role.name}** foi adicionada.`)
     msg.delete({ timeout: config.selfDestructMessageTimeoutMs })
 })
 
-export const removeRoleHandler: CommandHandler = checkRole(async (message, role) => {
-    if (!message.member!.roles.cache.array().includes(role)) {
+export const removeRoleHandler: CommandHandler = checkRole(async (message, role, user) => {
+    if (!user.roles.cache.array().includes(role)) {
         return fail(message, `Você não possui a role **${role.name}**`)
     }
 
-    await message.member!.roles.remove(role)
+    await user.roles.remove(role)
     await message.delete()
 
     const msg = await message.channel.send(`A role **${role.name}** foi removida.`)
